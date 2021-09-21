1b43d350ce84dc88b15b0bcada2472ec93de1324
Mr_Crafter <80324603+MrCrafterGlitched@users.noreply.github.com>
2021-09-21 16:42:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,10 +2,22 @@ function setup() {
   canvas = createCanvas(300, 300);
   canvas.center();
   video = createCapture(VIDEO);
-  //video.hide();
+  video.hide();
+  Classifier=ml5.imageClassifier("MobileNet",model_loaded)
+}
+function model_loaded() {
+  console.log("Model is loaded.")
 }
 function draw() {
-  image(video,0,0,300,300)
+ image(video,0,0,300,300)
+ Classifier.classify(video,getResults)
 }
-
-
+function getResults(error,results) {
+  if (error) {
+    console.log(error)
+  }else{
+    console.log(results)
+    document.getElementById("Object_name").innerHTML=results[0].label
+    document.getElementById("Object_accuracy").innerHTML=results[0].confidence.toFixed(3)
+  }
+}
